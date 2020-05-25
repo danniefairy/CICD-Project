b73a8f64877031c26f4fe09194d7bb35875dc6b9
Dannie <dannienctu@gmail.com>
2020-05-25 15:03:10 +0800

    update

--- scripts/prepare_config_file.bat
+++ scripts/prepare_config_file.bat
@@ -1,10 +1,8 @@
 SET ARG=%1
 
-pushd "d:/cygwin64/home/SYSTEM/ansible"
 if "%ARG%"=="stage"(
-    D:\cygwin64\bin\bash --login -c "ansible-playbook stage.yml -vvv"
+    D:\cygwin64\bin\bash --login -c "cd ansible && ansible-playbook stage.yml -vvv"
 )
 else (
-    D:\cygwin64\bin\bash --login -c "ansible-playbook prodction.yml -vvv"
+    D:\cygwin64\bin\bash --login -c "cd ansible && ansible-playbook prodction.yml -vvv"
 )
-popd
