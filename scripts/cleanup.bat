30922107c75ff3e12c460423da0e872ad3950713
Dannie <dannienctu@gmail.com>
2020-05-25 13:48:04 +0800

    update

--- scripts/cleanup.bat
+++ scripts/cleanup.bat
@@ -1,4 +1,4 @@
 :: The script will clean up the ansible dir and containers if the pipeline fails.
-rmdir d:\cygwin64\home\user\ansible /s /q
-rmdir d:\cygwin64\home\user\_build /s /q
+rmdir d:\cygwin64\home\SYSTEM\ansible /s /q
+rmdir d:\cygwin64\home\SYSTEM\_build /s /q
 docker rm -f stage_env prod_env
