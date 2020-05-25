70e1cdf4e9134eefc411bf10ef0e05e6a6aa27ef
Dannie <dannienctu@gmail.com>
2020-05-25 15:07:09 +0800

    update

--- scripts/prepare_config_file.bat
+++ scripts/prepare_config_file.bat
@@ -1,8 +1,10 @@
 SET ARG="%1"
 
-::if "%ARG%"=="stage"(
-::    D:\cygwin64\bin\bash --login -c "cd ansible && ansible-playbook stage.yml -vvv"
-::)
-::else (
-::    D:\cygwin64\bin\bash --login -c "cd ansible && ansible-playbook prodction.yml -vvv"
-::)
+if "%ARG%"=="stage"(
+    ::D:\cygwin64\bin\bash --login -c "cd ansible && ansible-playbook stage.yml -vvv"
+    echo "stage"
+)
+else (
+    ::D:\cygwin64\bin\bash --login -c "cd ansible && ansible-playbook prodction.yml -vvv"
+    echo "production"
+)
