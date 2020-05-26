e83d2c532fb141723882f3ebb50f37d1cdb9a0ea
Dannie <dannienctu@gmail.com>
2020-05-26 09:45:26 +0800

    stage

--- scripts/cleanup.bat
+++ scripts/cleanup.bat
@@ -1,4 +1,4 @@
 :: The script will clean up the ansible dir and containers if the pipeline fails.
 rmdir d:\cygwin64\home\SYSTEM\ansible /s /q
 rmdir d:\cygwin64\home\SYSTEM\_build /s /q
-docker rm -f $(docker ps |grep 'server')
+docker ps --filter name=server* -aq | xargs docker stop | xargs docker rm
