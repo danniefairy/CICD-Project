c1d45585727fd006240bdac09e1c9d61dd42114b
Dannie <dannienctu@gmail.com>
2020-05-26 11:12:54 +0800

    stage

--- server/message/message_test.go
+++ server/message/message_test.go
@@ -3,7 +3,7 @@ package message
 import "testing"
 
 func TestAbc(t *testing.T) {
-	if GetMessage() != "Hello World!!!" {
+	if GetMessage() != "Hello World" {
 		t.Errorf("Fail to get message")
 	}
 }
